924085155f0c1cdce83320c9683c7fd5bab6aa4d
MurilloRBS <104856741+MurilloRBS@users.noreply.github.com>
2024-07-31 14:36:43 +0000

    vvhj

---
+++ main.js
@@ -0,0 +1,44 @@
+const textos = document.querySelectorAll(".aba-conteudo");
+
+
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2030-12-31T23:59:59");
+
+const tempos = [tempoObjetivo1];
+
+
+function atualizaCronometro(){ 
+    for (let i=0; i<contadores.length;i++){
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
+
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    if (tempoFinal > 0){
+        return [dias,horas,minutos,segundos]
+    } else {
+        return [0,0,0,0];
+    }
+}
